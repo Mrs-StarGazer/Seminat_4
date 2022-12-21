374d4971e6ff4c15f74e592ba9c536f8611b4df4
Maria <m.shklyaeva96@gmail.com>
2022-12-22 00:24:17 +0500

    add task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень");
+int pow = int.Parse(Console.ReadLine()!);
+WrongPower();
+
+void WrongPower()
+{
+    int result = 1;
+    for (int i = 0; i < pow; i++)
+    {
+        result = result * number;
+    }
+    Console.WriteLine(result);
+}
+
+int Power(int a, int b)
+{
+    if (b < 0)
+    {
+        b = -b;
+    }
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
