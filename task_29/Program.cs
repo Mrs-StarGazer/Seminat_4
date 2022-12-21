fa10254c7c11a26823c97906f3fc9e91db3a8d43
Maria <m.shklyaeva96@gmail.com>
2022-12-22 01:01:54 +0500

    add task 29

---
+++ task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число N");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число MIN");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число MAX");
+int c = int.Parse(Console.ReadLine()!);
+
+int[] array = GenArray(a, b, c);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+int[] GenArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
